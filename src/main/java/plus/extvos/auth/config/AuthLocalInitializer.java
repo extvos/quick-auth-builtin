4de74cd786bc35670f5f7fcfd1fe7a09e1f026c6
Mingcai SHEN <archsh@gmail.com>
2022-01-22 22:14:32 +0800

    Create default user and role, perm.

--- src/main/java/plus/extvos/auth/config/AuthLocalInitializer.java
+++ src/main/java/plus/extvos/auth/config/AuthLocalInitializer.java
@@ -9,6 +9,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import plus.extvos.auth.entity.*;
+import plus.extvos.auth.service.*;
+import plus.extvos.common.utils.QuickHash;
 import plus.extvos.restlet.utils.DatabaseHelper;
 
 import javax.sql.DataSource;
@@ -26,11 +28,26 @@ public class AuthLocalInitializer implements ApplicationRunner {
     @Value("${quick.auth.default-user.username:admin}")
     private String defaultUsername;
 
-    @Value("${quick.auth.default-user.password:quick123}")
+    @Value("${quick.auth.default-user.password:quickstart}")
     private String defaultPassword;
 
-    @Value("${quick.auth.default-user.nickname:quick123}")
+    @Value("${quick.auth.default-user.nickname:quick-admin}")
     private String defaultNickname;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PermissionService permissionService;
+
+    @Autowired
+    private RolePermissionService rolePermissionService;
+
+    @Autowired
+    private UserRoleService userRoleService;
 
 
     @Override
@@ -51,6 +68,24 @@ public class AuthLocalInitializer implements ApplicationRunner {
         if (dh.tableAbsent(tableNames) > 0) {
             dh.runScriptsIfMySQL("sql/mysql/0.auth-schema.sql");
             dh.runScriptsIfPostgreSQL("sql/pg/0.auth-schema.sql");
+            Permission perm = new Permission();
+            perm.setCode("admin");
+            perm.setName("Administration");
+            perm.setComment("Administration");
+            permissionService.insert(perm);
+            Role role = new Role();
+            role.setCode("administrators");
+            role.setName("Administrators");
+            role.setComment("Administrators");
+            roleService.insert(role);
+            rolePermissionService.insert(new RolePermission(role.getId(), perm.getId()));
+            User user = new User();
+            user.setUsername(defaultUsername);
+            user.setNickname(defaultNickname);
+            user.setStatus((short)1);
+            user.setPassword(QuickHash.md5().hash(defaultPassword).hex());
+            userService.insert(user);
+            userRoleService.insert(new UserRole(user.getId(), role.getId()));
         }
     }
 }
