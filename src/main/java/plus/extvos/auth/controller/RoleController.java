900732d6aa171d46593ed7ca05156a6dfc04df32
Mingcai SHEN <archsh@gmail.com>
2022-09-20 09:18:35 +0800

    Updated: add permissions when get role.

--- src/main/java/plus/extvos/auth/controller/RoleController.java
+++ src/main/java/plus/extvos/auth/controller/RoleController.java
@@ -9,13 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import plus.extvos.auth.entity.Permission;
 import plus.extvos.auth.entity.Role;
+import plus.extvos.auth.entity.RolePermission;
 import plus.extvos.auth.service.PermissionService;
+import plus.extvos.auth.service.RolePermissionService;
 import plus.extvos.auth.service.RoleService;
 import plus.extvos.common.exception.ResultException;
 import plus.extvos.restlet.controller.BaseController;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 角色数据表
@@ -34,8 +37,8 @@ public class RoleController extends BaseController<Role, RoleService> {
     @Autowired
     private PermissionService permissionService;
 
-//    @Autowired
-//    private RolePermissionService rolePermissionService;
+    @Autowired
+    private RolePermissionService rolePermissionService;
 
     @Override
     public RoleService getService() {
@@ -48,6 +51,28 @@ public class RoleController extends BaseController<Role, RoleService> {
         super.postInsert(entity);
     }
 
+    protected Map<Integer, List<Permission>> getRolePermissions(Integer... ids) {
+        Map<Integer, List<Permission>> m = new HashMap<>();
+        if (ids.length < 1) {
+            return m;
+        }
+        QueryWrapper<RolePermission> qw1 = new QueryWrapper<>();
+        qw1.in("role_id", Arrays.asList(ids));
+        List<RolePermission> urs = rolePermissionService.selectByWrapper(qw1);
+        if (urs.size() < 1) {
+            return m;
+        }
+        QueryWrapper<Permission> qw2 = new QueryWrapper<>();
+        qw2.in("id", urs.stream().map(RolePermission::getPermissionId).collect(Collectors.toList()));
+        Map<Integer, Permission> roleMap = permissionService.selectByWrapper(qw2).stream().peek(o -> o.setUpdated(null)).peek(o -> o.setCreated(null)).collect(Collectors.toMap(Permission::getId, o -> o));
+        for (RolePermission ur : urs) {
+            List<Permission> rs = m.getOrDefault(ur.getRoleId(), new LinkedList<>());
+            rs.add(roleMap.get(ur.getPermissionId()));
+            m.put(ur.getRoleId(), rs);
+        }
+        return m;
+    }
+
     @Override
     public void postUpdate(Serializable id, Role entity) throws ResultException {
         super.postUpdate(id, entity);
@@ -55,35 +80,18 @@ public class RoleController extends BaseController<Role, RoleService> {
 
     @Override
     public Role postSelect(Role entity) throws ResultException {
-        QueryWrapper<Permission> qw = new QueryWrapper<>();
-        qw.inSql("id", "SELECT permission_id FROM builtin_role_permissions WHERE role_id = " + entity.getId());
-        entity.setPermissions(permissionService.selectByWrapper(qw).toArray(new Permission[0]));
+        Map<Integer, List<Permission>> pm = getRolePermissions(entity.getId());
+        entity.setPermissions(pm.getOrDefault(entity.getId(), new LinkedList<>()).toArray(new Permission[0]));
         return super.postSelect(entity);
     }
 
     @Override
     public List<Role> postSelect(List<Role> entities) throws ResultException {
-//        List<Integer> roleIds = new ArrayList<>();
-//        entities.forEach(e -> roleIds.add(e.getId()));
-//        Map<Integer, List<Permission>> rolePerms = new HashMap<>();
-//        List<RolePermission> rpms = rolePermissionService.getPermissionsByRoles(roleIds);
-//        rpms.forEach(rp -> {
-//            List<Permission> lp;
-//            if (rolePerms.containsKey(rp.getRoleId())) {
-//                lp = rolePerms.get(rp.getRoleId());
-//            } else {
-//                lp = new ArrayList<>();
-//            }
-//            lp.add(rp.getPermission());
-//            rolePerms.put(rp.getRoleId(), lp);
-//        });
-//        entities.forEach(e -> {
-//            if (rolePerms.containsKey(e.getId())) {
-//                List<Permission> lp = rolePerms.get(e.getId());
-//                Permission[] perms = new Permission[lp.size()];
-//                e.setPermissions(perms);
-//            }
-//        });
+        Integer[] ids = entities.stream().map(Role::getId).toArray(Integer[]::new);
+        Map<Integer, List<Permission>> permissionMap = getRolePermissions(ids);
+        entities.forEach(e -> {
+            e.setPermissions(permissionMap.getOrDefault(e.getId(), new LinkedList<>()).toArray(new Permission[0]));
+        });
         return super.postSelect(entities);
     }
 }
